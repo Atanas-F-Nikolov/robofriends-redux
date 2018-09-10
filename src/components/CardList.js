2118a567d760040033e58c59983ce2e4f46f6c87
Atanas-F-Nikolov <atanas.nikolov@itch.se>
2018-09-10 20:36:03 +0200

    bug fix,  which compromises img lazyloading

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
 const CardList = ({ robots }) => {
   return (
     <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
+      {robots.map(robot => {
+        return (
+          <Card
+            key={robot.id}
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default CardList;
