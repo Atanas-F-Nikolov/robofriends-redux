657585c6b876d7c235df91b0bad7fa0caf7dbff6
Atanas-F-Nikolov <atanas.nikolov@itch.se>
2018-09-10 20:23:58 +0200

    add robot placeholder img

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
+import robotPlaceholder from "../assets/img/robot-placeholder.png";
 
 const Card = ({ name, email, id }) => {
   return (
     <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
       <img
         alt="robots"
+        src={robotPlaceholder}
         data-src={`https://robohash.org/${id}?size=200x200`}
         className="lazyload"
       />
